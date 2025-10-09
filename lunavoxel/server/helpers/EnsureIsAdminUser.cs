d54ba4d23a96222e26259a56b50bb305ef096eea
Tyler Fritz <tfritzy@gmail.com>
2025-10-09 09:13:01 -0500

    A version of rls that sort of works

--- lunavoxel/server/helpers/EnsureIsAdminUser.cs
+++ lunavoxel/server/helpers/EnsureIsAdminUser.cs
@@ -5,7 +5,7 @@ public static class EnsureIsAdminUser
     public static void Check(SpacetimeDB.ReducerContext ctx)
     {
         var callerIdentity = ctx.Sender.ToString();
-        var isDev = callerIdentity.ToLower() == "c200a28354e4351f9e8b65bead363ad7d25e53dd13b5a036182790112a4ff801";
+        var isDev = callerIdentity.ToLower() == "c2005d5bae00d1820837474e0ef37fed49e107642493f99199c3f3911f6085ad";
         var isProd = callerIdentity.ToLower() == "c200a66ecc0876a0e621dc5a47de270237aaac6d3ba3c2915f490aeabcd9c599";
 
         if (!isDev && !isProd)
